02575c2be3c5312037698c746a2693609bf6b2b3
Mark R <mkrichey@gmail.com>
2023-12-27 15:15:11 +0000

    Learning variables and flex layouts. WIP

--- MapandFilters/python.py
+++ MapandFilters/python.py
@@ -2,10 +2,9 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Works up the stack
-avg = map(
+avg = sum(map(
         lambda n: n + 5,
         filter(lambda n: n % 2 == 0,numbers)
-    )
+    ))
 
-for m in avg:
-    print(m)
+print(avg)
