5de9ea88190a374b12d2d2329d59f0bd2b5c6dd1
Mark R <mkrichey@gmail.com>
2023-12-27 16:26:12 +0000

    Updated as changed by mistake

--- MapandFilters/python.py
+++ MapandFilters/python.py
@@ -2,9 +2,10 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Works up the stack
-avg = sum(map(
+avg = map(
         lambda n: n + 5,
         filter(lambda n: n % 2 == 0,numbers)
-    ))
+    )
 
-print(avg)
+for a in avg:
+    print(a)
